3f8bbfc686380842b9a8a7cea399bf9976ec46d7
SwarupKharul <swarupkharul@pop-os.localdomain>
2023-02-28 02:31:03 +0530

    feat: add contract

---
+++ contracts/ICC.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ICC is ERC721 {
+    uint256 public constant TOTAL_TOKEN_SUPPLY = 10000;
+    uint256 public tokenCount;
+    mapping(address => uint256) public nftTokenBalances;
+    mapping(address => uint256) public TokenBalances;
+
+    mapping(address => bool) public whitelist;
+    uint256 public tokenId;
+
+    constructor() ERC721("TicketingProtocol", "TICKET") {
+        tokenId = 0;
+        tokenCount = 0;
+    }
+
+    function buyTicket(uint256 price, uint256 numberofTickets)
+        external
+        payable
+    {
+        require(!whitelist[msg.sender], "You have already bought a ticket");
+        require(tokenCount + price < TOTAL_TOKEN_SUPPLY, "No tokens left");
+        // require(msg.value == price, "Incorrect payment amount");
+
+        tokenCount += price;
+        (bool success, ) = msg.sender.call{value: msg.value}("");
+        require(success, "Token transfer to buyer failed.");
+        tokenId++;
+        _mint(msg.sender, tokenId);
+        nftTokenBalances[msg.sender] = price;
+
+        whitelist[msg.sender] = true;
+    }
+
+    function buyTokens(uint256 price) external payable {
+        // require(msg.value > 0, "Insufficient funds sent");
+        require(tokenCount + price < TOTAL_TOKEN_SUPPLY, "No tokens left");
+        tokenCount += price;
+
+        TokenBalances[msg.sender] += price;
+    }
+
+    function transferToProtocol(address to, uint256 tokenId) public {
+        require(
+            _isApprovedOrOwner(msg.sender, tokenId),
+            "You are not the owner of this token"
+        );
+
+        uint256 unusedTokens = nftTokenBalances[msg.sender];
+        TokenBalances[to] += unusedTokens;
+    }
+
+    function buyWithTokens(address seller, uint256 amount) public {
+        require(TokenBalances[msg.sender] >= amount, "Insufficient funds");
+        require(
+            TokenBalances[seller] + amount > TokenBalances[seller],
+            "Overflow"
+        );
+
+        TokenBalances[msg.sender] -= amount;
+        TokenBalances[seller] += amount;
+    }
+
+    function withdraw() external payable {
+        uint256 amount = TokenBalances[msg.sender];
+        require(amount > 0, "Insufficient balance");
+        tokenCount -= amount;
+        TokenBalances[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+    }
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,22 +5,20 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const ICC = await hre.ethers.getContractFactory("ICC");
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const icc = await ICC.deploy();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await icc.deployed();
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  fs.writeFileSync(
+    "./config.js",
+    `export const contractAddress = "${icc.address}";`
   );
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
